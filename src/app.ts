45868a4a89fa39584ee935cb6f345b9985a892dc
siyam79 <siyamahmed3827@gmail.com>
2024-11-23 14:01:54 +0600

    Root route declear

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import cors from 'cors';
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import { ProductRoutes } from './app/modules/product/product.route';
 import { OrderRouters } from './app/modules/order/order.route';
 import errorHandler from './app/middleware/error.middleware';
@@ -13,6 +13,12 @@ app.use(cors());
 // application routes
 app.use('/api', ProductRoutes);
 app.use('/api', OrderRouters);
+
+
+
+app.get("/", (req: Request, res :Response) => {
+    res.status(200).json({ message: "Welcome to Stationery Shop Server" });
+});
 
 // Error-handling middleware
 app.use(errorHandler);
